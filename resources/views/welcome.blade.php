a5d7589934646de72fcfcd17942886569ae9d1a5
MTristan <53493044+MTristan012@users.noreply.github.com>
2023-07-14 00:22:51 -0600

    A1.0

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('main')
+<h1>Hola</h1>
+{{--@livewire('admin-permissions-filter')--}}
+@endsection
+
+@section('sidebar')
+<h1>Side</h1>
+{{--@livewire('admin-permissions-filter')--}}
+@endsection
